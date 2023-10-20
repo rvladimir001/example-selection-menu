ee73fc3e129ec898637a48dbb44ee61c9ef0d291
Vladimir <rvladimir001@gmail.com>
2023-10-20 13:01:34 +0300

    initial app and add memu

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Multiple Choice App
+        </q-toolbar-title>
+
+        <div></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  // setup() {
+  //
+  //   return {
+  //
+  //   };
+  // },
+});
+</script>
